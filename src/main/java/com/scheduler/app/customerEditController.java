5b32cb3d8a3b8cd04285e76e4ae3ce86386ec8c4
xlar54 <scott.hutter@gmail.com>
2022-12-31 09:50:56 -0600

    update combo boxes dynamicall

--- src/main/java/com/scheduler/app/customerEditController.java
+++ src/main/java/com/scheduler/app/customerEditController.java
@@ -1,14 +1,22 @@
 package com.scheduler.app;
 
+import com.scheduler.dao.CountryDAO;
 import com.scheduler.dao.CustomerDAO;
+import com.scheduler.dao.FirstLevelDivisionDAO;
+import com.scheduler.pojo.Country;
 import com.scheduler.pojo.CustomerRow;
+import com.scheduler.pojo.FirstLevelDivision;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -30,8 +38,25 @@ public class customerEditController implements Initializable {
 
 
     @Override
+        public void initialize(URL location, ResourceBundle resources) {
+
+        initTableView();
+
+        try {
+            initCountryComboBox();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            refreshTableViewData();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     @SuppressWarnings("unchecked")
-    public void initialize(URL location, ResourceBundle resources) {
+    private void initTableView() {
 
         TableColumn<CustomerRow, Integer> customerIDCol       = new TableColumn<CustomerRow, Integer>("ID");
         customerIDCol.setCellValueFactory(new PropertyValueFactory<CustomerRow, Integer>("Customer_ID"));
@@ -54,24 +79,80 @@ public class customerEditController implements Initializable {
         TableColumn<CustomerRow, String> customerCountryCol    = new TableColumn<CustomerRow, String>("Country");
         customerCountryCol.setCellValueFactory(new PropertyValueFactory<CustomerRow,String>("Country"));
 
-
         customersTableView.getColumns().addAll(customerIDCol,customerNameCol,customerAddressCol,
                 customerPostalCol, customerPhoneCol, customerDivisionCol, customerCountryCol);
 
-        try {
-            refreshTableViewData();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        // Here is a lambda method as requested on document
+        customersTableView.setOnMouseClicked((MouseEvent event) -> {
+            onCustomerTableViewSelectedRow();
+        });
+    }
+
+    /// Initializes the Country combo box from SQl and
+    /// sets the listeners for change events
+    private void initCountryComboBox() throws Exception {
+
+        cboCountry.getItems().clear();
+
+        CountryDAO countryDAO = new CountryDAO();
+        ArrayList<Country> countries = countryDAO.getAll();
+
+        for (Country country : countries) cboCountry.getItems().add(country.getCountry());
+
+        // create handler for change to country combobox
+        cboCountry.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                String selectedCountry = (String) cboCountry.getValue();
+                try {
+                    onCountryComboBoxChange(selectedCountry);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+    private void onCountryComboBoxChange(String selectedCountry) throws Exception {
+
+        // loads the divisions when the country combobox is changed
+        cboDivision.getItems().clear();
+
+        FirstLevelDivisionDAO fldDAO = new FirstLevelDivisionDAO();
+        ArrayList<FirstLevelDivision> flds = fldDAO.getByCountryName(selectedCountry);
+
+        for (FirstLevelDivision fld : flds) cboDivision.getItems().add(fld.getDivision());
 
     }
 
     private void refreshTableViewData() throws Exception {
+
+        customersTableView.getItems().clear();
 
         CustomerDAO customerDAO = new CustomerDAO();
         ObservableList<CustomerRow> customers = customerDAO.getCustomerRows();
 
         customersTableView.getItems().addAll(customers);
 
+    }
+
+    public void onCustomerTableViewSelectedRow() {
+
+        if (customersTableView.getSelectionModel().getSelectedItem() != null) {
+
+            // Get selected row
+            CustomerRow selectedRow = customersTableView.getSelectionModel().getSelectedItem();
+
+            // populate the UI fields
+            lblCustomerID.setText(String.valueOf(selectedRow.getCustomer_ID()));
+            txtCustomerName.setText(selectedRow.getCustomer_name());
+            txtAddress.setText(selectedRow.getAddress());
+            txtPhoneNumber.setText(selectedRow.getPhone());
+            txtPostalCode.setText(selectedRow.getPostal_code());
+
+            // set the comboboxes
+            cboCountry.setValue((String)selectedRow.getCountry());
+            cboDivision.setValue((String)selectedRow.getDivision());
+        }
     }
 }
