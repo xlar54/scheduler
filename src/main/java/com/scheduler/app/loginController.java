b85a5dbecb910e78a9079611153c2fbc288c5e55
xlar54 <scott.hutter@gmail.com>
2022-12-31 17:19:23 -0600

    form login and chaining

--- src/main/java/com/scheduler/app/loginController.java
+++ src/main/java/com/scheduler/app/loginController.java
@@ -2,10 +2,13 @@ package com.scheduler.app;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
@@ -13,6 +16,7 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.TimeZone;
@@ -21,14 +25,15 @@ import javafx.scene.control.TableView;
 
 import com.scheduler.pojo.*;
 import com.scheduler.dao.UserDAO;
+import javafx.stage.WindowEvent;
 
 public class loginController implements Initializable {
     @FXML
-    Button signInButton;
+    Button btnSignIn;
     @FXML
-    TextField userNameTextField;
+    TextField txtUsername;
     @FXML
-    TextField passwordTextField;
+    TextField txtPassword;
     @FXML
     Label zoneID = new Label();
     @FXML
@@ -37,96 +42,42 @@ public class loginController implements Initializable {
     Label passWordLabel = new Label();
     @FXML
     Label CustomerLogin = new Label();
+
+    @FXML
+    Button btnEditCustomers;
+    @FXML
+    Button btnEditAppointments;
     Boolean french = false;
-
-    @FXML
-    ObservableList<ObservableList> data;
-    @FXML
-    Label col1Row1 = new Label();
-    @FXML
-    Label col1Row2 = new Label();
-    @FXML
-    Label col1Row3 = new Label();
-    @FXML
-    Label col1Row4 = new Label();
-    @FXML
-    Label col1Row5 = new Label();
-    @FXML
-    Label col1Row6 = new Label();
-    @FXML
-    Label col1Row7 = new Label();
-
-    @FXML
-    Label col1Row8 = new Label();
-
-
-
-    @FXML
-    Label col2Row1 = new Label();
-    @FXML
-    Label col2Row2 = new Label();
-    @FXML
-    Label col2Row3 = new Label();
-    @FXML
-    Label col2Row4 = new Label();
-    @FXML
-    Label col2Row5 = new Label();
-    @FXML
-    Label col2Row6 = new Label();
-    @FXML
-    Label col2Row7 = new Label();
-    @FXML
-    Label col2Row8 = new Label();
-
-
-
-    @FXML
-    TextField c1R1 = new TextField();
-    @FXML
-    TextField c1R2 = new TextField();
-    @FXML
-    TextField c1R3= new TextField();
-    @FXML
-    TextField c1R4 = new TextField();
-    @FXML
-    TextField c1R5 = new TextField();
-    @FXML
-    TextField c1R6 = new TextField();
-    @FXML
-    TextField c1R7 = new TextField();
-
-    @FXML
-    TextField c1R8 = new TextField();
-    @FXML
-    TextField c2R1 = new TextField();
-    @FXML
-    TextField c2R2 = new TextField();
-    @FXML
-    TextField c2R3 = new TextField();
-    @FXML
-    TextField c2R4 = new TextField();
-    @FXML
-    TextField c2R5 = new TextField();
-    @FXML
-    TextField c2R6 = new TextField();
-    @FXML
-    TextField c2R7 = new TextField();
-    @FXML
-    TextField c2R8 = new TextField();
-
-
-
-
-    @FXML
-    Button addButton = new Button();
-    @FXML
-    Button updateButton =new Button();
-    @FXML
-    Button deleteButton =new Button();
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        if(App.loggedInUser != null)
+            enableMenu();
+        else
+            enableLogin();
+
+        btnSignIn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    btnSignInHandler(event);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        btnEditCustomers.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    openCustomerEditForm();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
 
     }
 
@@ -144,7 +95,7 @@ public class loginController implements Initializable {
                     .getText(Translation.Language.FRENCH, Translation.LanguageKey.USERNAME_LABEL));
             passWordLabel.setText(Translation.getInstance()
                     .getText(Translation.Language.FRENCH, Translation.LanguageKey.PASSWORD_LABEL));
-            signInButton.setText(Translation.getInstance()
+            btnSignIn.setText(Translation.getInstance()
                     .getText(Translation.Language.FRENCH, Translation.LanguageKey.SIGN_IN_BUTTON));
 
             y= 1;
@@ -154,10 +105,10 @@ public class loginController implements Initializable {
     }
 
     @FXML 
-    void signInHandler(ActionEvent event) throws Exception {
+    void btnSignInHandler(ActionEvent event) throws Exception {
 
-        String enteredUsername = userNameTextField.getText();
-        String enteredPassword = passwordTextField.getText();
+        String enteredUsername = txtUsername.getText();
+        String enteredPassword = txtPassword.getText();
 
         User user = new User();
         UserDAO userDAO = new UserDAO();
@@ -182,6 +133,15 @@ public class loginController implements Initializable {
 
             a.showAndWait();
 
+            btnEditCustomers.setVisible(true);
+            btnEditAppointments.setVisible(true);
+
+            userNameLabel.setVisible(false);
+            passWordLabel.setVisible(false);
+            txtUsername.setVisible(false);
+            txtPassword.setVisible(false);
+            btnSignIn.setVisible(false);
+
         }
         else
         {
@@ -190,13 +150,25 @@ public class loginController implements Initializable {
                     .getText(Translation.Language.ENGLISH, Translation.LanguageKey.LOGIN_TRY_AGAIN));
             a.showAndWait();
         }
+    }
 
-        Stage stage = (Stage) signInButton.getScene().getWindow();
+    void openCustomerEditForm() throws IOException {
+
+        Stage stage = (Stage) btnSignIn.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/customerEdit.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 500, 640);
         Scene scene = new Scene(fxmlLoader.load(), 800, 550);
         stage.setTitle("Schedule Application");
 
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                try {
+                    closebackuptaskandshowmaintask(we);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
         // center on screen
         Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
         stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
@@ -205,13 +177,57 @@ public class loginController implements Initializable {
 
     }
 
+    private void closebackuptaskandshowmaintask(Event event) throws Exception {
+        // Decalaration of Variables
+        Stage stage, stage1;
+        String eventstring;
 
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/login.fxml"));
+        stage = (Stage) btnEditCustomers.getScene().getWindow();
 
-    @FXML 
-    void signUpHandler(ActionEvent event)
-    {
-        String passWord = passwordTextField.getText();
-        String userName = userNameTextField.getText();
+        // Execution Block
+        eventstring = event.getEventType().toString();
+        if ("ACTION".equals(eventstring)) {
+
+            stage.close();
+        } else if ("WINDOW_CLOSE_REQUEST".equals(eventstring)) {
+            event.consume();
+            stage = (Stage) event.getSource();
+            stage.close();
+        }
+
+        Scene scene = new Scene(fxmlLoader.load(), 600, 250);
+
+        stage1 = new Stage();
+        stage1.setScene(scene);
+        stage1.setTitle("Schedule Application");
+        stage1.show();
+    }
+
+    void enableMenu() {
+
+        btnEditCustomers.setVisible(true);
+        btnEditAppointments.setVisible(true);
+
+        userNameLabel.setVisible(false);
+        passWordLabel.setVisible(false);
+        txtUsername.setVisible(false);
+        txtPassword.setVisible(false);
+        btnSignIn.setVisible(false);
+
+    }
+
+    void enableLogin() {
+
+        btnEditCustomers.setVisible(false);
+        btnEditAppointments.setVisible(false);
+
+        userNameLabel.setVisible(true);
+        passWordLabel.setVisible(true);
+        txtUsername.setVisible(true);
+        txtPassword.setVisible(true);
+        btnSignIn.setVisible(true);
+
     }
 }
     
