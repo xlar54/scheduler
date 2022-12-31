48f111729f0eec4142fce8bcdf1e6868eb9076da
xlar54 <scott.hutter@gmail.com>
2022-12-30 18:47:04 -0600

    customer list populate from db

--- src/main/java/com/scheduler/app/customerEditController.java
+++ src/main/java/com/scheduler/app/customerEditController.java
@@ -6,12 +6,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -19,25 +17,25 @@ import java.util.ResourceBundle;
 
 public class customerEditController implements Initializable {
     @FXML
-    private TableView customersTableView;
+    private TableView<Customer> customersTableView;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        TableColumn customerIDCol       = new TableColumn("ID");
+        TableColumn<Customer, Integer> customerIDCol       = new TableColumn<Customer, Integer>("ID");
         customerIDCol.setCellValueFactory(new PropertyValueFactory<Customer, Integer>("Customer_ID"));
 
-        TableColumn customerNameCol     = new TableColumn("Name");
+        TableColumn<Customer, String> customerNameCol     = new TableColumn<Customer, String>("Name");
         customerNameCol.setCellValueFactory(new PropertyValueFactory<Customer,String>("Customer_name"));
 
-        TableColumn customerAddressCol  = new TableColumn("Address");
+        TableColumn<Customer, String> customerAddressCol  = new TableColumn<>("Address");
         customerAddressCol.setCellValueFactory(new PropertyValueFactory<Customer,String>("Address"));
 
-        TableColumn customerPostalCol   = new TableColumn("Postal Code");
+        TableColumn<Customer, String> customerPostalCol   = new TableColumn<>("Postal Code");
         customerPostalCol.setCellValueFactory(new PropertyValueFactory<Customer,String>("Postal_code"));
 
-        TableColumn customerPhoneCol    = new TableColumn("Phone");
+        TableColumn<Customer, String> customerPhoneCol    = new TableColumn<Customer, String>("Phone");
         customerPhoneCol.setCellValueFactory(new PropertyValueFactory<Customer,String>("Phone"));
 
         customersTableView.getColumns().addAll(customerIDCol,customerNameCol,customerAddressCol,
